6fc1ef5b0a1f66500bc70d5f7276bec79883e02d
Rylie <rylie.stone2005@gmail.com>
2024-04-24 12:49:46 -0400

    wrote all request methods and made database useable

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,37 @@
-const getAll = () => {
+const db = require('../../data/db-config')
+const getAll = async () => {
   // DO YOUR MAGIC
+  const accounts = await db('accounts')
+  return accounts
 }
 
-const getById = id => {
+const getById = async id => {
   // DO YOUR MAGIC
+  const account = await db('accounts').where('id', id).first()
+  return account
 }
 
-const create = account => {
+const create = async account => {
   // DO YOUR MAGIC
+  const {name, budget} = account
+  const req = await db('accounts').insert({name: name.trim(), budget})
+  console.log(req)
+  const newAccount = await getById(req)
+  return newAccount
+ }
+
+const updateById = async (id, account) => {
+  // DO YOUR MAGIC
+  const req = await db('accounts').where('id', id).update(account)
+  const updatedAccount = await getById(req)
+  return updatedAccount
 }
 
-const updateById = (id, account) => {
+const deleteById = async id => {
   // DO YOUR MAGIC
-}
-
-const deleteById = id => {
-  // DO YOUR MAGIC
+  const account = await getById(id)
+  await db('accounts').where('id', id).delete()
+  return account
 }
 
 module.exports = {
